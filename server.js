2650285bf83dac818810cd6348db2751bac250cd
risx <rrrisx@gmail.com>
2017-08-09 22:47:01 -0700

    master catchup before rework

--- server.js
+++ server.js
@@ -3,25 +3,25 @@
 const express = require('express');
 const app = express();
 
-// const io = require('socket.io');
-// const server = require('http').createServer(app);
-// const socket = io.listen(server);
+const io = require('socket.io');
+const server = require('http').createServer(app);
+const socket = io.listen(server);
 
 const port = 3000;
 
-// let users = 0;
+let users = 0;
 
 app.use(express.static(__dirname + '/public'));
 
-// socket.on('connection', function(socket){
-//   users++;
-//   console.log(users, 'connected');
-//   socket.on('disconnect', function(){ 
-//     users--;
-//     console.log(users, 'connected');
-//   });
-// });
+socket.on('connection', function(socket){
+  users++;
+  console.log(users, 'connected');
+  socket.on('disconnect', function(){ 
+    users--;
+    console.log(users, 'connected');
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Open on localhost:' + port);
 });
